7c1a9fe7434003f9898ad8cd05859ab0e4447848
Lika <14dubinenkoanzhelika@gmail.com>
2023-06-12 20:45:02 +0300

    add first logic

--- cards-tweets/src/components/Card/Card.jsx
+++ cards-tweets/src/components/Card/Card.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./card.module.scss";
-import user from "../../images/user.png";
+import user from "../../images/Hansel.png";
 import logo from "../../images/Logo.png";
 
 const Card = () => {
+  const [newFollower, setNewFollower] = useState(true);
+
+  const handleFollow = () => setNewFollower(!newFollower);
+
   return (
     <div className={styles.card}>
       <div className={styles.background}>
@@ -15,19 +19,26 @@ const Card = () => {
           height={22}
         />
       </div>
+      <div className={styles.wrapimage}>
+        <img
+          className={styles.image}
+          src={user}
+          alt="User"
+          width={60}
+          height={60}
+        />
+      </div>
 
-      <img
-        className={styles.image}
-        src={user}
-        alt="User"
-        width={80}
-        height={80}
-      />
       <p className={styles.tweets}>777 tweets</p>
-      <p className={styles.followers}>100,500 Followers</p>
+      <p className={styles.followers}>
+        {newFollower ? "100,500 " : "100,501 "}Followers
+      </p>
       <div className={styles.wrapbtn}>
-        <button className={styles.btn} type="button">
-          Follow
+        <button
+          className={newFollower ? `${styles.btn}` : `${styles.btnActive}`}
+          type="button"
+          onClick={handleFollow}>
+          {newFollower ? "Follow " : "Following "}
         </button>
       </div>
     </div>
